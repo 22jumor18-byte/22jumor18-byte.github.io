420fa8727681a45eb443299ce99222513caf597b
22jumor18-byte <22jumor18@gmail.com>
2026-07-08 13:30:01 -0700

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+const button=document.getElementById("button");
+const textbox=document.getElementById("textbox");
+
+
+button.addEventListener("click",() => {
+
+    textbox.textContent="help me";
+});
